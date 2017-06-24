b2d12569546097da8ccb0be190b7181d1bba1fed
Ken Johnson <kenjtel@yahoo.com>
2017-06-23 18:19:11 -0700

    run thru jshint

--- fibonacciNumbers.js
+++ fibonacciNumbers.js
@@ -1,19 +1,19 @@
 // print fibonacci numbers, using loop and not recursion
-
+"use strict";
 var first = 0;
 var second = 1;
 var temp = 0;
 
-N = 100;
+var N = 100;
 
 console.log(0);
 
-while ( second < N ) {
-   
-    if(first > 0 )	console.log(second);
-   
+while (second < N) {
+
+	if (first > 0) console.log(second);
+
 	temp = first + second;
 	first = second;
 	second = temp;
-	   	
+
 }
