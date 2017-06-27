3c0e93323e073b3c96927b4de389a35a2228f49b
Ken Johnson <kenjtel@yahoo.com>
2017-06-26 17:20:52 -0700

    ran jshint

--- fibonacciByRecursion.js
+++ fibonacciByRecursion.js
@@ -1,13 +1,14 @@
 // print the fibonacci numbers using recursion
+"use strict";
 var N = 100;
 
-console.log(0); 
+console.log(0);
 
-function fib(a,b){
-	if (a+b >= N) return;
-	console.log(a+b);
-	fib(b,a+b);
+function fib(a, b) {
+	if (a + b >= N) return;
+	console.log(a + b);
+	fib(b, a + b);
 
 }
 
-fib(0,1);
+fib(0, 1);
